732ebf4d667e9856e7768ad855469a52d1cc5060
Joseph Lee <josephlee3004@gmail.com>
2024-07-03 15:08:15 -0400

    Create work experience preview section

---
+++ src/components/JobPreview.jsx
@@ -0,0 +1,13 @@
+function JobPreview({ job }) {
+  return (
+    <div>
+      <p>Company: {job.company}</p>
+      <p>Position: {job.position}</p>
+      <p>Responsibilities: {job.responsibilities}</p>
+      <p>Start date: {job.start}</p>
+      <p>End date: {job.end}</p>
+    </div>
+  );
+}
+
+export default JobPreview;
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,12 +1,16 @@
 import PersonalPreview from './PersonalPreview';
 import EducationPreview from './EducationPreview';
+import JobPreview from './JobPreview';
 
-function Preview({ personalInfo, educations, setSubmitted }) {
+function Preview({ personalInfo, educations, jobs, setSubmitted }) {
   return (
     <div>
       <PersonalPreview info={personalInfo} />
       {educations.map((education) => {
         return <EducationPreview key={education.id} education={education} />;
+      })}
+      {jobs.map((job) => {
+        return <JobPreview key={job.id} job={job} />;
       })}
       <button type="button" onClick={() => setSubmitted(false)}>
         Edit
